e58f032bf11299947a5af4d6f9a5668da543c624
Andrey <siehdd@mail.ru>
2020-08-13 20:57:02 +0300

    fix bugs

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import { createContext, useContext } from 'react';
 import mixpanel from 'mixpanel-browser';
 
-
 export const MixPanelContext = createContext();
 export const useMixPanel = () => useContext(MixPanelContext);
 
-export const MixPanelProvider = ({ children }) => <MixPanelContext.Provider value={mixpanel}>{children}</MixPanelContext.Provider>;
+export const MixPanelProvider = ({ children }) => (
+  <MixPanelContext.Provider value={mixpanel}>{children}</MixPanelContext.Provider>
+);
 
-export default Mixpanel = mixpanel;
+const Mixpanel = mixpanel;
+
+export default Mixpanel;
